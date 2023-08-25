2546b8fdea8665ece5c6c9746598bc56bb3786f6
abhinav khanna <kabhinav99@gmail.com>
2023-08-26 00:58:44 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Routes ,Route} from "react-router-dom";
-import { Navbar } from './components/navbar/navbar.jsx';
+import { Navbar } from './components/navbar.jsx';
 import { Cart } from './pages/cart/cart';
 import { Shop } from './pages/shop/shop';
 import { ShopcontextProvider } from './context/shop-context';
